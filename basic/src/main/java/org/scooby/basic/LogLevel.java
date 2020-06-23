80377a142d48aa5c1aac46b6c5d77e96a2098244
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2020-06-23 23:39:14 +0300

    LogLevel class

---
+++ basic/src/main/java/org/scooby/basic/LogLevel.java
@@ -0,0 +1,56 @@
+package org.scooby.basic;
+
+public class LogLevel {
+    private int priority;
+    private String name;
+    
+    public LogLevel(int priority, String name) {
+        this.priority = priority;
+        this.name = name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + priority;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LogLevel other = (LogLevel) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (priority != other.priority)
+            return false;
+        return true;
+    }
+    
+}
